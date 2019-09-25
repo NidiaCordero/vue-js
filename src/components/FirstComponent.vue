60f346058a4ac40495ebc658819ec635e449c428
Cordero Nidia <ruizcorderonidia@gmail.com>
2019-09-25 12:07:38 +0200

    started themplate boostratp

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Titre h1</h1>
+    <p>Hello {{ msg }}</p>
+    <p>Seulement  {{ fullTeext }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+    msg:'test',
+      hello: "hello",
+      word:"word",
+      fullText:"Hello Word"
+    };
+  },
+  computed: {
+    fullTeext() {
+      return this.hello + ' ' + this.word;
+    }
+  }
+};
+</script>
+<style>
+</style>
